2089392ff18b3180d9fa971d781e0276bc414db9
norfolkak <st130153@student.spbu.ru>
2025-03-27 23:02:06 +0300

    Removed unnecessary commentary.

--- test.cpp
+++ test.cpp
@@ -13,15 +13,12 @@ void decodeStream();
 
 constexpr std::array<unsigned int, 5> POW85{ 85*85*85*85, 85*85*85, 85*85, 85, 1 };
 
-// Тестируем буферный режим кодирования и обратное декодирование
 TEST(Ascii85Test, EncodeDecodeBuffered) 
 {
-    // Исходные данные
     std::string inputStr = "Man";
     std::istringstream input(inputStr);
     std::ostringstream encodedOutput;
     
-    // Перенаправляем потоки
     auto cinbuf = std::cin.rdbuf();
     auto coutbuf = std::cout.rdbuf();
     std::cin.rdbuf(input.rdbuf());
@@ -30,11 +27,9 @@ TEST(Ascii85Test, EncodeDecodeBuffered)
     // Кодирование
     encodeBuffered();
     
-    // Восстанавливаем потоки
     std::cin.rdbuf(cinbuf);
     std::cout.rdbuf(coutbuf);
     
-    // Декодирование
     std::istringstream encodedInput(encodedOutput.str());
     std::ostringstream decodedOutput;
     std::cin.rdbuf(encodedInput.rdbuf());
@@ -45,11 +40,9 @@ TEST(Ascii85Test, EncodeDecodeBuffered)
     std::cin.rdbuf(cinbuf);
     std::cout.rdbuf(coutbuf);
     
-    // Ожидаем, что декодированный результат совпадает с исходными данными
     EXPECT_EQ(decodedOutput.str(), inputStr);
 }
 
-// Тестируем потоковый режим кодирования и обратное декодирование
 TEST(Ascii85Test, EncodeDecodeStream) {
     std::string inputStr = "TestData";
     std::istringstream input(inputStr);
@@ -78,9 +71,6 @@ TEST(Ascii85Test, EncodeDecodeStream) {
     EXPECT_EQ(decodedOutput.str(), inputStr);
 }
 
-//
-// Точка входа для тестов Google Test
-//
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
